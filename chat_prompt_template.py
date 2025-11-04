b119bc57c19f6c32909fdc12b517332d7b038d50
Sharad <sharad.rajore@zensar.com>
2025-11-04 12:53:27 +0530

    concept of PromptTemplate and ChatPromptTemplate

---
+++ chat_prompt_template.py
@@ -0,0 +1,19 @@
+from langchain_ollama import ChatOllama
+from langchain_core.prompts import ChatPromptTemplate
+
+
+prompt = ChatPromptTemplate.from_template("Tell me a key achivements of {person} in 4 bulleted points in {category}?")
+
+#from dotenv import load_dotenv
+
+#load_dotenv()
+
+llm = ChatOllama(model="llama3.2:latest")
+
+chain = prompt | llm 
+
+response = chain.invoke({"person":"Rahul Gandhi","category":"politics"})
+
+print(response.content)
+
+
