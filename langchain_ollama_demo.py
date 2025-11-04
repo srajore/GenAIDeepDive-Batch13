bed7423e90bff1fe3227a1ce006bae651e82472a
Sharad <sharad.rajore@zensar.com>
2025-11-04 10:11:34 +0530

    added support for ollama models

---
+++ langchain_ollama_demo.py
@@ -0,0 +1,13 @@
+
+from langchain_ollama import ChatOllama
+#from dotenv import load_dotenv
+
+#load_dotenv()
+
+llm = ChatOllama(model="llama3.2:latest")
+
+response = llm.invoke("Could you explain what is Agentic AI in simple terms?")
+
+print(response.content)
+
+
