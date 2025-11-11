d595cb04807d162decc44e1aae6155a8fe7cb3a6
Sharad <sharad.rajore@zensar.com>
2025-11-11 12:06:11 +0530

    used azure ai foundry

---
+++ azure_demo.py
@@ -0,0 +1,22 @@
+from langchain_openai import AzureChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
+
+# Initialize AzureOpenAI for text completion
+llm = AzureChatOpenAI(
+    deployment_name="openai_deployment",  # Replace with your actual deployment name
+    model="gpt-4.1",
+    openai_api_type="azure",
+    api_version=os.getenv("AZURE_OPENAI_API_VERSION")
+)
+
+prompt = PromptTemplate.from_template("What is the capital of {country}?")
+
+chain = prompt | llm
+
+response = chain.invoke({"country": "India"})
+
+print(response.content)
